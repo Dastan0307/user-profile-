2f0455bcf14cd1005cdd1a1081a58eeacbe0b7b7
Dastan <nurgazyuuludastan@gmail.com>
2024-07-09 01:34:02 +0600

    add header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,14 +1,36 @@
 import Image from 'next/image'
+import avatar from '../../../public/Avatar.png'
 import logo from '../../../public/Logo.svg'
+import vector from '../../../public/Union.svg'
+import burger from '../../../public/burger.svg'
 
 const Header = () => {
-  return (
-	<header>
-		<div>
-			<Image src={logo} alt='logo' width={180} height={40} />
-		</div>
-	</header>
-  )
+	return (
+		<header className='w-full flex items-center justify-between'>
+			<div>
+				<Image src={logo} alt='logo' width={180} height={40} />
+			</div>
+			<div className='w-[128px] flex items-center justify-between'>
+				<div className='w-[80px] h-[36px] flex justify-center items-center border-1 border-custom rounded-18'>
+					<Image src={avatar} alt='logo' width={24} height={24} />
+					<Image
+						src={vector}
+						alt='logo'
+						width={14}
+						height={8}
+						className='cursor-pointer  m-[8px_5px]'
+					/>
+				</div>
+				<Image
+					src={burger}
+					alt='logo'
+					width={24}
+					height={24}
+					className='cursor-pointer m-[8px]'
+				/>
+			</div>
+		</header>
+	)
 }
 
 export default Header
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,15 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      borderWidth: {
+        '1': '1px',
+      },
+      borderColor: {
+        'custom': 'rgb(54, 55, 56)',
+      },
+      borderRadius: {
+        '18': '18px',
+      },
     },
   },
   plugins: [],
